92a0d3790d4c13fec9144b9b1fb005b147c0ecc0
ByLuisen <luisenric32@gmail.com>
2025-07-03 16:48:16 +0200

    Build phase scripts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  bloodyKneeApiUrl: '__BLOODY_KNEE_API_URL__',
-  domain: '__DOMAIN__',
-  SPAClientId: '__SPA_CLIENT_ID__',
+  bloodyKneeApiUrl: '',
+  domain: '',
+  SPAClientID: '',
 };

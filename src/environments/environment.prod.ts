d0fc1544666a319200a53314981d2ac94932a392
ByLuisen <luisenric32@gmail.com>
2025-07-04 15:48:08 +0200

    Solving naming error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   bloodyKneeApiUrl: '__BLOODY_KNEE_API_URL__',
   domain: '__DOMAIN__',
-  SPAClientId: '__SPA_CLIENT_ID__',
+  SPAClientID: '__SPA_CLIENT_ID__',
 };

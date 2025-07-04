0e87609ef427390d89d100a503d0925b262903b9
ByLuisen <luisenric32@gmail.com>
2025-07-04 16:13:08 +0200

    Reundo placeholder

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -5,8 +5,8 @@ const envFile = path.join(__dirname, "../src/environments/environment.prod.ts");
 let content = fs.readFileSync(envFile, "utf8");
 
 content = content
-  .replace(/%%BLOODY_KNEE_API_URL$$/g, process.env.BLOODY_KNEE_API_URL || "")
-  .replace(/%%DOMAIN%%/g, process.env.DOMAIN || "")
-  .replace(/%%SPA_CLIENT_ID%%/g, process.env.SPA_CLIENT_ID || "");
+  .replace(/__BLOODY_KNEE_API_URL__/g, process.env.BLOODY_KNEE_API_URL || "")
+  .replace(/__DOMAIN__/g, process.env.DOMAIN || "")
+  .replace(/__SPA_CLIENT_ID__/g, process.env.SPA_CLIENT_ID || "");
 
 fs.writeFileSync(envFile, content, "utf8");
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  bloodyKneeApiUrl: '%%BLOODY_KNEE_API_URL%%',
-  domain: '%%DOMAIN%%',
-  SPAClientID: '%%SPA_CLIENT_ID%%',
+  bloodyKneeApiUrl: '__BLOODY_KNEE_API_URL__',
+  domain: '__DOMAIN__',
+  SPAClientID: '__SPA_CLIENT_ID__',
 };
